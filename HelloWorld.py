dec3391077c8c5c202eae47bd8e362ea19989e94
ZunLayNwe12 <zunlaynwe123456@gmail.com>
2019-12-15 20:10:17 +0630

    OpenCV1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,7 @@
 print("HelloWorld")
 
 1 2 3 4 5
+
+# this is the first comment
+spam = 1 # and this is th esecond comment # ... and now a third!
+text = "# This is not a comment because it's inside quote."
